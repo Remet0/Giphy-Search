33b6cffdc47aed99061e54859fcd868323c043fa
Remeto <remetojohn@gmail.com>
2019-07-02 14:56:47 -0700

    updated the ajax call so it had no errors. Added to the response to have it populate the page with 10 gifs and ratings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,14 +16,28 @@ $('button').on('click', function(){
     //get the data attribute for the button clicked
     let input = $(this).attr('data-name');
     //api key for giphy t3X8WSh5UuP13eOL33cqlZmOKV3kFp0W
-    let queryURL = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=t3X8WSh5UuP13eOL33cqlZmOKV3kFp0W`;
+    let queryURL = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=t3X8WSh5UuP13eOL33cqlZmOKV3kFp0W&limit=10`;
     
     $.ajax({
-        URL: queryURL,
+        url: queryURL,
         method: 'GET'
     }).then(function(response){
             console.log(response);
-            $('.gif').append(JSON.stringify(response));
+            let results = response.data;
+        for (let i = 0; i < results.length; i++) {
+                let gifDiv = $('<Div>');
+                console.log(results[i].images.fixed_height.url);
+                let rating = results[i].rating;
+                //sets up a element with the rating to be appended later
+                let p = $('<p>');
+                p.text(`Rating: ${rating}`);
+                //sets up a element with the gif image to be appended later
+                let gif = $('<img>');
+                gif.attr('src', results[i].images.fixed_height.url);
+            //append both new elements
+                gifDiv.append(p).append(gif);
+                $('.gif').prepend(gifDiv);
+        }
     })
 
 
