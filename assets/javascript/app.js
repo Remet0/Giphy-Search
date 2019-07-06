bb8d6af8e1ecb40662435712cd5c571b1f534c8d
Remeto <remetojohn@gmail.com>
2019-07-05 17:23:39 -0700

    added a like button the the pictures and updates ajvascript to make them work. Changed some margins and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-let searchTerms = ['justice league', 'shrek', 'deadpool'];
+let searchTerms = ['justice league', 'deadpool', 'shrek', 'avengers'];
 
 
 
@@ -36,7 +36,7 @@ $('.buttons').on('click', 'button', function(){
     //get the data attribute for the button clicked
     let input = $(this).attr('data-name');
     //api key for giphy t3X8WSh5UuP13eOL33cqlZmOKV3kFp0W
-    let queryURL = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=t3X8WSh5UuP13eOL33cqlZmOKV3kFp0W&limit=10`;
+    let queryURL = `https://api.giphy.com/v1/gifs/search?q=${input}&api_key=t3X8WSh5UuP13eOL33cqlZmOKV3kFp0W`;
 
     //ajax call to the input URL to get database information and display it
     $.ajax({
@@ -44,9 +44,8 @@ $('.buttons').on('click', 'button', function(){
         method: 'GET'
     }).then(function(response){
             let results = response.data;
-        for (let i = 0; i < results.length; i++) {
+        for (let i = 0; i < 10; i++) {
                 let gifDiv = $('<Div>');
-            console.log(response);
                 let rating = results[i].rating;
                 //sets up a element with the rating to be appended later
                 let p = $('<p>');
@@ -54,12 +53,15 @@ $('.buttons').on('click', 'button', function(){
                 //sets up a element with the gif image to be appended later
                 let gif = $('<img>');
                 gif.attr('src', results[i].images.fixed_height_still.url).attr('id', 'gifActive').attr('data-inactive', results[i].images.fixed_height.url);
+                let favorite = $('<a>');
+                favorite.text('Like');
+                favorite.attr('href', '#').attr('data-inactive', results[i].images.fixed_height.url).attr('data-number', results[i].images.fixed_height_still.url).attr('data-id', i);
             //append both new elements
-                gifDiv.append(p).append(gif);
+                gifDiv.append(p).append(gif).append(favorite);
                 $('.gif').prepend(gifDiv);
         }
-    })
-})
+    });
+});
 //on click function to swap still and active gifs
 $('.gif').on('click', '#gifActive', function(){
     let src = $(this).attr('src');
@@ -67,7 +69,24 @@ $('.gif').on('click', '#gifActive', function(){
     $(this).attr('src', $(this).attr('data-inactive'));
 
     $(this).attr('data-inactive', src);
+});
+$('.gif').on('click', 'a', function(){
+    $(this).toggleClass('active');
     
+    if($(this).hasClass('active')){  
+        let favoriteImg = $('<img>');
+         favoriteImg.attr('src', $(this).attr('data-number')).attr('data-inactive', $(this).attr('data-inactive')).attr('data-id', $(this).attr('data-id'));
+         $('.favorites').append(favoriteImg);
+    }else{
+        let removeImg = $(this).attr('data-id');
+        $(`.favorites img[data-id="${removeImg}"]`).remove();
+    }
+});
 
+//adds pausing functionality to favorited images
+$('.favorites').on('click', 'img', function(){
+    let src = $(this).attr('src');
+    $(this).attr('src', $(this).attr('data-inactive'));
 
-})
+    $(this).attr('data-inactive', src);
+});
